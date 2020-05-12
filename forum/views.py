88dccdebac663cbf546e60b25b0c916b4f6b6f37
NikitaLobaev <nikitos2255@yandex.ru>
2020-05-12 16:35:57 +0300

    Оптимизирована пагинация списка вопросов.

--- forum/views.py
+++ forum/views.py
@@ -1,16 +1,17 @@
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.http import HttpResponseNotFound, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views.decorators.http import require_GET
 
 from forum.models import Question, QuestionTag, Profile
 
+# TODO: optimize and sort ALL imports EVERYWHERE!
 questions_per_page = 3
 users_per_page = 10
 
 
-def questions(request, questions_):
+def questions_pagination(request, questions_):
 	try:  # TODO: может быть, добавить limit на количество вопросов на странице
 		order = Question.manager.accept_order[request.GET.get('order', next(iter(Question.manager.accept_order)))]
 		paginator = Paginator(questions_.order_by(order), questions_per_page)
@@ -27,16 +28,14 @@ def questions(request, questions_):
 
 
 def index(request):
-	return questions(request, Question.manager)
+	return questions_pagination(request, Question.manager)
 
 
 @require_GET
 def user(request, user_id):
-	tags = QuestionTag.manager.all()
 	return render(request, 'user.html', {
-		'user': Profile.manager.get_by_id(user_id),  # get_object_or_404(Profile, id=user_id)
-		'tags_left': tags[:int((len(tags) + 1) / 2)],
-		'tags_right': tags[int((len(tags) + 1) / 2):]
+		'user': get_object_or_404(Profile, id=user_id),
+		'tags': QuestionTag.manager.all()
 	})
 
 
@@ -49,11 +48,9 @@ def users(request):
 		profiles = paginator.page(1)
 	except EmptyPage:
 		profiles = paginator.page(paginator.num_pages)
-	tags = QuestionTag.manager.all()
 	return render(request, 'users.html', {
 		'profiles': profiles,
-		'tags_left': tags[:int((len(tags) + 1) / 2)],
-		'tags_right': tags[int((len(tags) + 1) / 2):]
+		'tags': QuestionTag.manager.all()
 	})
 
 
@@ -70,16 +67,14 @@ def question(request, question_id):
 		question_ = Question.manager.get_by_id(question_id)
 	except Question.DoesNotExist:
 		return HttpResponseNotFound()
-	tags = QuestionTag.manager.all()
 	return render(request, 'question.html', {
 		'question': question_,
-		'tags_left': tags[:int((len(tags) + 1) / 2)],
-		'tags_right': tags[int((len(tags) + 1) / 2):]
+		'tags': QuestionTag.manager.all()
 	})
 
 
 def hot(request):
-	return questions(request, Question.manager.get_hot())
+	return questions_pagination(request, Question.manager.get_hot())
 
 
 def tag(request, tag_name):
@@ -87,4 +82,4 @@ def tag(request, tag_name):
 		questions_ = Question.manager.get_by_tag(tag_name)
 	except QuestionTag.DoesNotExist:
 		return HttpResponseNotFound()
-	return questions(request, questions_)
+	return questions_pagination(request, questions_)
